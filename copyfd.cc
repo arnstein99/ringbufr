d2c9cf92bfd2e97682fb406b88fbee33fc2c8b46
David Arnstein <arnstein@pobox.com>
2022-06-12 23:35:02 -0700

    Fix more logic errors in copyfd.

--- copyfd.cc
+++ copyfd.cc
@@ -7,6 +7,10 @@
 #include <unistd.h>
 
 // #define VERBOSE
+#define CHECKPOINT \
+    do { \
+        /* std::cerr << "checkpoint " << __LINE__ << std::endl; */ \
+    } while (false)
 
 size_t copyfd(int readfd, int writefd, size_t chunk_size)
 {
@@ -19,72 +23,95 @@ size_t copyfd(int readfd, int writefd, size_t chunk_size)
     ssize_t bytes_read = 0;
     ssize_t bytes_write = 0;
     size_t bytes_processed = 0;
+    size_t read_available;
+    size_t write_available;
     do
     {
 	fd_set* p_read_set = nullptr;
 	fd_set* p_write_set = nullptr;
 
 	unsigned char* read_start;
-	size_t read_available;
 	bufr.pushInquire(read_available, read_start);
+        CHECKPOINT;
+        bytes_read = 0;
 	if (read_available)
 	{
+            CHECKPOINT;
 	    read_available = std::min(read_available, chunk_size);
-	    FD_ZERO(&read_set);
-	    FD_SET(readfd, &read_set);
-	    p_read_set = &read_set;
+            bytes_read = read(readfd, read_start, read_available);
+            if (bytes_read < 0)
+            {
+                if ((errno == EWOULDBLOCK) || (errno == EAGAIN))
+                {
+                    FD_ZERO(&read_set);
+                    FD_SET(readfd, &read_set);
+                    p_read_set = &read_set;
+                }
+                else
+                {
+		    std::cerr << "read: " << strerror(errno) << std::endl;
+                    break;
+                }
+            }
+            else if (bytes_read == 0)
+            {
+                // End of input
+                break;
+            }
+            else
+            {
+                bufr.push(bytes_read);
+            }
 	}
 
 	bytes_write = 0;
 	unsigned char* write_start;
-	size_t write_available;
 	bufr.popInquire(write_available, write_start);
 	if (write_available)
 	{
+            CHECKPOINT;
 	    bytes_write = write(writefd, write_start, write_available);
 	    if (bytes_write < 0)
 	    {
+                CHECKPOINT;
 		if ((errno == EWOULDBLOCK) || (errno == EAGAIN))
 		{
+                    CHECKPOINT;
 		    FD_ZERO(&write_set);
 		    FD_SET(writefd, &write_set);
 		    p_write_set = &write_set;
 		}
 		else
 		{
-		    std::cerr << "write " << strerror(errno) << std::endl;
+                    CHECKPOINT;
+		    std::cerr << "write: " << strerror(errno) << std::endl;
 		    break;
 		}
 	    }
-	    else
+            else if (bytes_write == 0)
+            {
+                CHECKPOINT;
+                std::cerr << "copyfd: EOF on write descriptor" << std::endl;
+                break;
+            }
+            else
 	    {
+                CHECKPOINT;
 	        bufr.pop(bytes_write);
 		bytes_processed += bytes_write;
 	    }
 	}
 
-        int select_return = 
-	    select(maxfd, p_read_set, p_write_set, nullptr, nullptr);
-	if (select_return < 0)
-	{
-	    std::cerr << "select " << strerror(errno) << std::endl;
-	    continue;
-	}
-
-	bytes_read = 0;
-	if (p_read_set && FD_ISSET(readfd, p_read_set))
-	{
-	    bytes_read = read(readfd, read_start, read_available);
-	    if (bytes_read < 0)
-	    {
-		std::cerr << "read " << strerror(errno) << std::endl;
-		break;
-	    }
-	    else
-	    {
-	        bufr.push(bytes_read);
-	    }
-	}
+        if (p_read_set || p_write_set)
+        {
+            int select_return = 
+                select(maxfd, p_read_set, p_write_set, nullptr, nullptr);
+            if (select_return < 0)
+            {
+                std::cerr << "select: " << strerror(errno) << std::endl;
+                continue;
+            }
+        }
 
 #ifdef VERBOSE
 	std::cerr << "read " << bytes_read << " write " << bytes_write <<
@@ -92,6 +119,7 @@ size_t copyfd(int readfd, int writefd, size_t chunk_size)
 #endif
 
     } while (bytes_read || bytes_write);
+    // Includes negative values, meaning select() was just called.
 
     return bytes_processed;
 }
